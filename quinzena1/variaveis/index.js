1fbd85c4d2cde3b1db3988ac884fd9989940bc79
Alessandra Barreto da Silva <r.alessandra.b@gmail.com>
2021-05-20 23:04:56 -0300

    variaveis e exercicios

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,98 @@
+//Exercícios de escrita de código
+
+/*
+1.
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)*/
+// vai aparecer: 10, 10, 5 
+
+
+/*
+2.
+let a = 10 Declara o valor da variavel a como 10
+let b = 20 Declara o valor da variavel b como 20
+let c Declara a variavel sem  valor 
+c = a altera o valor da variavel c para o valor da variavel a
+b = c altera o valor da variavel b para o valor da variavel c
+a = b altera o valor da variavel a para o valor da variavel b
+
+
+console.log(a, b, c) imprime os valores das variaveis a, b, c   10 10 10 */
+
+
+
+/* 
+3.
+let horasTrabalhoDia = prompt("Quantas horas você trabalha por dia?") 
+let salarioDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${horasTrabalhoDia/salarioDia} por hora`)*/
+
+
+
+
+//### Exercícios de escrita de código
+/* 1.
+const nome =
+const idade =
+console.log (typeof(nome), (typeof(idade)) undefined  Pq não foi declarado nenhum valor 
+const nome = prompt("Qual o seu nome?")
+const idade = number(prompt("Qual a sua idade?")
+
+nome = prompt ("Qual o seu nome")
+idade = Number (prompt ("Qual a sua idade"))
+
+console.log (typeof (nome), typeof (idade))
+console.log("Olá",nome, "você tem",idade, "anos".) */
+
+
+
+//2.
+/*
+let estaDor = "Você está com dor nas costas?"
+let estaSono = "Você está com sono agora?"
+let estaFome = "Você está com fome?"
+
+let respostaDor = "Não"
+let respostaSono = "Sim"
+let respotaFome = "Sim"
+
+console.log( estaDor,respostaDor,estaSono,respostaSono,estaFome,respotaFome) 
+*/
+
+
+
+
+//3. 
+/*
+let a = 10
+let b = 25
+
+b = a  // 10
+a = a + b + 5 // a10+10+5
+
+console.log("O novo valor de a é", a) 
+console.log("O novo valor de b é", b) 
+
+let a 
+let b 
+let c 
+
+
+a = Number (prompt("Digite um numero para a")) // 25
+b = Number (prompt("Digite um numero para b")) // 10
+
+c =a  //- c=25
+a=b  // - a=10
+b=a //- b=25
+
+console.log("O valor de a:",a,"O valor de b:",b)
+*/
+
+
+
+
