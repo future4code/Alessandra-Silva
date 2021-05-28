19d34733de7739c25bed7669a31f1519008e8955
Alessandra Barreto da Silva <r.alessandra.b@gmail.com>
2021-05-27 22:15:25 -0300

    exercicios de strings e arrays

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,28 @@
+// Exercícios de interpretação de código
+//1.
+//a. undefined - a array não foi declarada
+//b. a array foi declarada como nula, será executada como null
+//c. define o tamanho, quantos itens 11
+//d. imprime a primeira posição, que é 0
+//e. 
+//f. imprime a 6 posição, 9
+
+/*2.Toda a frase ficará em caixa alta, a letra A será 
+substituida por I e será contado quantos caracteres 
+(incluindo espaços) tem na frase. */
+
+//###################################//
+
+// Exercícios de escrita de código
+//1.
+/* const nomeUsuario = prompt("Qual seu nome?")
+const emailUsuario = prompt("Qual seu e-mail?")
+console.log("O e-mail " + emailUsuario + "  foi cadastrado com sucesso. Seja bem-vinda(o)," + nomeUsuario) */
+
+//2.
+
+const comidasPreferidas = ["lasanha", "strogonoff", "batata", "pipoca", "chocolate"]
+//a.
+console.log(comidasPreferidas);
+//b. 
+console.log("Essas são as minhas comidas preferidas: " + comidasPreferidas )
